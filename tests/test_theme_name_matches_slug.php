b2f58e0591b393ba1724082fe17c5482c1550234
Herb Miller <herb@bobbingwide.com>
2022-04-12 11:48:49 +0100

    Add comment about theme fixup routine #4

--- tests/test_theme_name_matches_slug.php
+++ tests/test_theme_name_matches_slug.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This isn't actually a test. It a fixup routine to correct oik-themes posts
+ * where the _oikth_slug was incorrectly set during a theme update.
+ * The values for the previously created theme were carried over in the $_POST array.
+ * Problem occurred on 2022/04/11
+ *
+ * @var
+ * $args
+ */
+
 $args = [ 'post_type' => 'oik-themes', 'numberposts' => -1];
 $posts = get_posts( $args );
 echo count( $posts );
